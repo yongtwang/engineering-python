9feb9ea49dd5ecd81ec622a84ff4cfdfc9651342
Yong Wang <34072409+yongtwang@users.noreply.github.com>
2023-11-07 11:27:27 -0500

    Update animation_3d.py

--- 15D_MatPlotLib_Gallery_Examples/animation_3d.py
+++ 15D_MatPlotLib_Gallery_Examples/animation_3d.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
 
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111,projection='3d')
@@ -10,7 +9,7 @@ X, Y = np.meshgrid(X, Y)
 wframe = None
 for phi in np.arange(360):  #360 degrees
     if wframe != None: # Remove old lines before drawing
-        ax.collections.remove(wframe)
+        wframe.remove()  # Revised on 11/7/2023
     Z = np.cos(2 * np.pi * X + phi) * (1 - np.sqrt(X**2 + Y**2))
     wframe = ax.plot_wireframe(X, Y, Z)
     ax.view_init(azim=phi, elev=50)
